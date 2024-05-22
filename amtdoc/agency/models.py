eaea9833eca4a52d5360dca360c1035d8ad02713
samuel.ojo <samuel.ojo@interswitchgroup.com>
2024-05-22 15:17:11 +0100

    Added User models in agency app

--- amtdoc/agency/models.py
+++ amtdoc/agency/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+GENDER_CHOICES = (
+	(0, 'male'),
+	(1, 'female'),
+)
+
+class User(AbstractUser):
+	pass
+
+class Lga(models.Model):
+	name = models.CharField(max_length=255)
+
+class Agent(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+	lga = models.ForeignKey(Lga, on_delete=models.CASCADE)
+	town = models.CharField(max_length=255)
+	phone = models.CharField(max_length=20)
+
+class Patient(models.Model):
+	gender = models.IntegerField(choices=GENDER_CHOICES)
+	dob = models.DateField()
+	phone = models.CharField(max_length=20, blank=True, null=True)
+	address = models.TextField()
+
+class Doctor(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+	name = models.CharField(max_length=255)
